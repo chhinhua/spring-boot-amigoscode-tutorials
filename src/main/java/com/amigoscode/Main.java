d9f471498b039b05b8bc19fcdc9e070444f97fa9
ziinHua <chhinhua@gmail.com>
2023-01-13 15:05:21 +0700

    Update

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -3,14 +3,21 @@ package com.amigoscode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Objects;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
@@ -24,6 +31,10 @@ public class Main {
         return responde;
     }
 
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
     record Person(String name, Integer age, double save){}
     record GreetResponde(String greet,
                          List<String> favProgramingLanguages,
